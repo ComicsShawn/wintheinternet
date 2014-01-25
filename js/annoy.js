8e42db1e665eede57420d6ff76e611288b3c316a
Shawn Smith <shawn@netstuffers.com>
2014-01-24 22:00:34 -0500

    Panel progress

--- js/annoy.js
+++ js/annoy.js
@@ -1,1 +1,7 @@
-alert("This is annoying");
+$(document).ready(function(){
+	$('body').append("<div id='wti_panel'></div>");
+	var p = $('#wti_panel');
+	$.get(chrome.extension.getURL("panel.html"), function(data){
+		p.html(data);
+	});
+});
