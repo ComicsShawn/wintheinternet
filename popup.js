2fc5b5eab97a805dfe9a5b6117c8c87aacf137fb
Shawn Smith <shawn@netstuffers.com>
2014-01-24 21:19:08 -0500

    Manifest and annoy

--- popup.js
+++ popup.js
@@ -3,15 +3,25 @@
 // found in the LICENSE file.
 
 
-function click(e) {
-  chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-  window.close();
+// The function below is executed in the context of the inspected page.
+var page_getProperties = function() {
+  var data = window.jQuery && $0 ? jQuery.data($0) : {};
+  // Make a shallow copy with a null prototype, so that sidebar does not
+  // expose prototype.
+  var props = Object.getOwnPropertyNames(data);
+  var copy = { __proto__: null };
+  for (var i = 0; i < props.length; ++i)
+    copy[props[i]] = data[props[i]];
+  return copy;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
+chrome.devtools.panels.elements.createSidebarPane(
+    "jQuery Properties",
+    function(sidebar) {
+  function updateElementProperties() {
+    sidebar.setExpression("(" + page_getProperties.toString() + ")()");
   }
+  updateElementProperties();
+  chrome.devtools.panels.elements.onSelectionChanged.addListener(
+      updateElementProperties);
 });
